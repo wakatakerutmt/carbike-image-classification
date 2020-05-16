d5ab0d396c8e07cbaca184b8e183525ea543db55
takuya.iida <wakatakerutmt@ezweb.ne.jp>
2020-05-17 05:50:42 +0900

    add index view

--- carbike/urls.py
+++ carbike/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'carbike'
 urlpatterns = [
     path('', views.index, name='index'),
     path('predict/', views.predict, name='predict')
--- carbike/views.py
+++ carbike/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .forms import PhotoForm
 
 def index(request):
-    return HttpResponse("hello")
+    template = loader.get_template('carbike/index.html')
+    context = {'form':PhotoForm()}
+    return HttpResponse(template.render(context, request))
 
 def predict(request):
     return HttpResponse("hellwwwo")
